b49800d14eb1a8de2d8294c612fc5963b1e5e2c3
khchoi0301 <khchoi0301@gmail.com>
2019-10-03 22:26:05 +0900

    add bootswatch

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,8 @@ const Layout = (props) => (
     <div>
         <Head>
             <title>BitzPrice</title>
+            <link rel='stylesheet'
+                href="https://bootswatch.com/4/cerulean/bootstrap.min.css" />
         </Head>
         <Navbar />
         {props.children}
